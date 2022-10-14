25a47e8f7962f678629648947f9063fdee292316
weldxit <113974251+weldxit@users.noreply.github.com>
2022-10-14 13:09:15 +0530

    Revert "Master"

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,85 +1,43 @@
-// import React from 'react';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-
-
-
-// export default function Header() {
-//   return (
-  
-
-  //       <Navbar bg="light" expand="lg">
-  //         <Container fluid>
-  //           <Navbar.Brand as = {Link} to ={"/Home"}>E-Mandi</Navbar.Brand>
-  //           <><Navbar.Toggle aria-controls="navbarScroll" /><Navbar.Collapse id="navbarScroll">
-  //                 <Nav
-  //                   className="me-auto my-2 my-lg-0"
-  //                   style={{ maxHeight: '100px' }}
-  //                   navbarScroll
-  //                 >
-  
-  //                   <Nav.Link as ={Link} to={'/Sell'}>Sell</Nav.Link>
-  //                   <Nav.Link as ={Link} to={'/Buy'}>Buy</Nav.Link>
-  //                   <Nav.Link as={Link} to={'/About'}>About</Nav.Link>
-  //                   <Nav.Link as={Link} to={'/Contact'}>Contact</Nav.Link>
-  //                 </Nav>
-  //                 <Form className="d-flex">
-//                   <Form.Control
-//                     type="search"
-//                     placeholder="Search"
-//                     className="me-2"
-//                     aria-label="Search" />
-//                   <Button variant="outline-success">Search</Button>
-//                 </Form>
-//                 <Nav.Link as={Link} to={'/Register'}>Login/Signup</Nav.Link>
-//               </Navbar.Collapse></>
-//         </Container>
-//       </Navbar>          
-//     );
-
-// }
-
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Link} from 'gatsby';
 
-function CollapsibleExample() {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand as ={Link} to='/Home'>E - mandi</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/Buy">Buy</Nav.Link>
-            <Nav.Link href="/Sell">Sell</Nav.Link>
-            {/* <NavDropdown title="Market Place" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Buy Products</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Sell your product
-              </NavDropdown.Item>
-              <NavDropdown.Divider />  
-              <NavDropdown.Item href="#action/3.3">Trusted Sellers</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to='/Buy'>
-               Verified Buyers
-              </NavDropdown.Item>
-            </NavDropdown> */}
-            <Nav.Link href="/About">About</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link as={Link} to='/Login'>Log In</Nav.Link>
-            <Nav.Link eventKey={2} as = {Link} to ='/Signup'>
-              Sign Up
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
 
-export default CollapsibleExample;
+
+export default function Header() {
+  return (
+  
+
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand as = {Link} to ={"/Home"}>E-Mandi</Navbar.Brand>
+          <><Navbar.Toggle aria-controls="navbarScroll" /><Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{ maxHeight: '100px' }}
+                  navbarScroll
+                >
+
+                  <Nav.Link as ={Link} to={'/Sell'}>Sell</Nav.Link>
+                  <Nav.Link as ={Link} to={'/Buy'}>Buy</Nav.Link>
+                  <Nav.Link as={Link} to={'/About'}>About</Nav.Link>
+                  <Nav.Link as={Link} to={'/Contact'}>Contact</Nav.Link>
+                </Nav>
+                {/* <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search" />
+                  <Button variant="outline-success">Search</Button>
+                </Form> */}
+                <Nav.Link as={Link} to={'/Register'}>Login/Signup</Nav.Link>
+              </Navbar.Collapse></>
+        </Container>
+      </Navbar>
+                  
+    );
+  
+}
--- src/pages/SellYour.jsx
+++ src/pages/SellYour.jsx
@@ -1,126 +1,93 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 import Form from 'react-bootstrap/Form';
-import BoxSx from './Box';
-import './css/box.css'
 
 export default function SellYour() {
-    return (
-        <div className="box">
-
-
-            <BoxSx>
-                <img src="https://unsplash.com/photos/7KLa-xLbSXA" alt="sfjlsakf" />
-            </BoxSx>
-
-
-            <BoxSx>
-
-                <div className="sign">
+  return (
+    <div className="sign">
 
 
 
-                    <form className='sig'>
-                        
-                        <div className="main">
+    <form className='sig'>
+        <h3 >Product Details</h3>
+        <div className="main">
 
-                            <div className='left'>
-                            <h4 >Product Details</h4>
-                                <div className="mb-3">
-                                    <label>Name</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Name of your product"
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label>Catagory</label>
-                                    <Form.Select aria-label="Catagory">
-                                        <option>Select Catagory</option>
-                                        <option value="0">Rice</option>
-                                        <option value="1">Dal</option>
-                                        <option value="2">Vegitables</option>
-                                        <option value="3">Mashroom</option>
-                                        <option value="4">Meat</option>
-                                        <option value="5">Desi Chicken</option>
-                                        <option value="6">Egg</option>
-                                    </Form.Select>
-                                </div>
-                                <div className="mb-3">
-                                    <label>Quantity</label>
-                                    <input
-                                        type="number"
-                                        className="form-control"
-                                        placeholder="e.g 10kg"
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label>Selling Price</label>
-                                    <input
-                                        type="number"
-                                        className="form-control"
-                                        placeholder="₹ 1,000"
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label>Block</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Enter Block"
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label>Dist</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Enter Dist"
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label>Image</label>
-                                    <input
-                                        type="file"
-                                        className="form-control"
-                                        placeholder="Upload an image folder"
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="d-grid">
-                            <button type="submit" className="btn btn-primary">
-                                Sign Up
-                            </button>
-                        </div>
-                        <p className="forgot-password text-right">
-                            Already registered <Link to='/Login'>Sign in</Link>
-                        </p>
-                    </form>
+        <div className='left'>
 
+        <div className="mb-3">
+            <label>Name</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Name of your product"
+            />
+        </div>
+        <div className="mb-3">
+            <label>Catagory</label>
+        <Form.Select aria-label="Catagory">
+      <option>Select Catagory</option>
+      <option value="0">Rice</option>
+      <option value="1">Dal</option>
+      <option value="2">Vegitables</option>
+      <option value="3">Mashroom</option>
+      <option value="4">Meat</option>
+      <option value="5">Desi Chicken</option>
+      <option value="6">Egg</option>
+    </Form.Select>
+        </div>
+        <div className="mb-3">
+            <label>Quantity</label>
+            <input
+                type="number"
+                className="form-control"
+                placeholder="e.g 10kg"
+                />
+        </div>
+        <div className="mb-3">
+            <label>Selling Price</label>
+            <input
+                type="number"
+                className="form-control"
+                placeholder="₹ 1,000"
+                />
+        </div>
+        <div className="mb-3">
+            <label>Block</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Block"
+                />
+        </div>
+        <div className="mb-3">
+            <label>Dist</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Dist"
+                />
+            </div>
+            <div className="mb-3">
+            <label>Image</label>
+            <input
+                type="file"
+                className="form-control"
+                placeholder="Upload an image folder"
+            />
+        </div>
+                </div>
+                </div>
+        <div className="d-grid">
+            <button type="submit" className="btn btn-primary">
+                Sign Up
+            </button>
+        </div>
+        <p className="forgot-password text-right">
+            Already registered <Link to='/Login'>Sign in</Link>
+        </p>
+    </form>
+    
 
                 </div>
-            </BoxSx>
-
-        </div>
-    )
+  )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
